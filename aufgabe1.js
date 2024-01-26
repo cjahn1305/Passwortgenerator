3fcfc44b4050fe668a68e4d0aa11b0cfb28759da
cjahn1305 <152269511+cjahn1305@users.noreply.github.com>
2024-01-26 14:54:52 +0100

    Add files via upload

---
+++ aufgabe1.js
@@ -0,0 +1,12 @@
+function aufgabe1(inputString) {
+    // Den String umkehren
+    var reversedString = inputString.split('').reverse().join('');
+    
+    // In der Konsole ausgeben
+    console.log(reversedString);
+  }
+  
+  // Beispielaufruf der Funktion
+  var input = "Hallo Erde, hier ist Alpha!";
+  aufgabe1(input);
+  
